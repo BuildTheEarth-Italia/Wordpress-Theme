de1a462bc96f2085ccb1fa60b855a7e0add1f971
rospino74 <34315725+rospino74@users.noreply.github.com>
2021-04-02 19:57:00 +0200

    fix(Scroll): Riparate animazioni navbar + titolo

--- js/parallax.js
+++ js/parallax.js
@@ -1,30 +1,65 @@
-jQuery(document).ready(($) => {
-    lax.setup(); //avvio lax
-    const nav = $(".mainNav"); //reference alla navbar
-    const main = $(".content"); //reference a main
+lax.setup(); // Inizializzo lax
 
-    const updateOnScroll = () => {
-        //aggiorno lax
-        lax.update(window.scrollY);
+const navbar = jQuery('.navbar');  // Reference alla navbar
+const container = jQuery('.root'); // Reference al contenuto
 
-        //distanza navbar da fine viewport
-        const navbarHeight = nav.get(0).getBoundingClientRect().bottom;
+currentMargin = parseInt(
+    container.css('marginTop')   // Margine di default di container
+);
 
-        //distanza tra main e fine viewport
-        const mainOffset = main.get(0).getBoundingClientRect().top;
+const updateOnScroll = function () {
+    // Assegnazione per compatibilità
+    const scroll = jQuery(document).scrollTop();
 
-        //a che altezza devo mostrare sfondo navbar?
-        const distance = mainOffset - navbarHeight;
-        
-        //aggiungo lo sfondo a navbar se necessario
-        if(distance <= 50) {
-            nav.addClass("custom-background");
+    // Aggiorno lax
+    lax.update(scroll);
+
+    // Ottengo la distanza tra la fine della navbar è il top della viewport
+    const distance = scroll - navbar.outerHeight(true);
+
+    // Se distance è maggiore di 0 aggiungo classe fixed
+    if (distance >= 0) {
+        // Aggiungo la classe fixed-top alla navbar
+        navbar.addClass('custom-background fixed');
+
+        // Aggiungo margine al container
+        container.css('marginTop', currentMargin + navbar.outerHeight(true));
+
+        // Imposto la distanza da il margine di sopra se la navbar può ancora scendere
+        if (distance <= navbar.outerHeight(true)) {
+            navbar.css('top', distance - navbar.outerHeight(true));
         } else {
-            nav.removeClass("custom-background");
+            navbar.css('top', 0);
         }
-        
-        //animo nuovamente
-        window.requestAnimationFrame(updateOnScroll);
+    } else {
+        // Rimuovo la classe fixed-top alla navbar
+        navbar.removeClass('csudtom-background fixed');
+
+        // Rimuovo margine al container
+        container.css('marginTop', currentMargin);
+
+        // Imposto la distanza da il margine di sopra a 0
+        navbar.css('marginTop', null);
     }
-    window.requestAnimationFrame(updateOnScroll);
-});
+}
+
+// Se la pagina viene ridimensionata aggiorno currentMargin
+const updateOnResize = function () {
+    container.css('marginTop', null); // Rimuovo il margine vecchio
+
+    currentMargin = parseInt(
+        container.css('marginTop') // Aggiorno il margine di default
+    );
+
+    // Aggiungo margine nuovo al container
+    container.css('marginTop', currentMargin + navbar.outerHeight(true));
+};
+
+
+// Animo ad ogni scorrimento della pagina
+window.addEventListener('scroll', () =>
+    window.requestAnimationFrame(updateOnScroll)
+);
+
+// Aggiorno le dimensioni in caso di ridimensionamento
+window.addEventListener('resize', updateOnResize);
